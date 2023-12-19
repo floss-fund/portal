b808b17f4e16c99117c4fb05f708f1e8ba4f89c9
Kailash Nadh <kailash@nadh.in>
2023-12-19 22:27:21 +0530

    Refactor error messages and fix ID regexp.

--- internal/validations/validations.go
+++ internal/validations/validations.go
@@ -11,13 +11,13 @@ import (
 
 var (
 	reTag = regexp.MustCompile(`^\p{L}([\p{L}\d-]+)?\p{L}$`)
-	reID  = regexp.MustCompile(`^[a-z]([[a-z]\d-]+)?[a-z]$`)
+	reID  = regexp.MustCompile(`^[a-z0-9]([[a-z\d-]+)?[a-z0-9]$`)
 )
 
 // InRange checks whether the given number > min and < max.
 func InRange[T int | int32 | int64 | float32 | float64](tag string, num T, min, max T) error {
 	if num < min || num > max {
-		return fmt.Errorf("%s should be of length %v - %v", tag, min, max)
+		return fmt.Errorf("`%s` should be of length %v - %v", tag, min, max)
 	}
 
 	return nil
@@ -26,9 +26,9 @@ func InRange[T int | int32 | int64 | float32 | float64](tag string, num T, min, 
 func InList[S ~[]E, E comparable](tag string, item E, items S) error {
 	if !slices.Contains(items, item) {
 		if s, ok := any(items).([]string); ok {
-			return fmt.Errorf("%s should be one of: %s", tag, strings.Join(s, ", "))
+			return fmt.Errorf("`%s` should be one of: %s", tag, strings.Join(s, ", "))
 		}
-		return fmt.Errorf("%s has an unknown value", tag)
+		return fmt.Errorf("`%s` has an unknown value", tag)
 	}
 
 	return nil
@@ -36,7 +36,7 @@ func InList[S ~[]E, E comparable](tag string, item E, items S) error {
 
 func InMap[M ~map[I]I, I comparable](tag, mapName string, item I, mp M) error {
 	if _, ok := mp[item]; !ok {
-		return fmt.Errorf("%s was not found in the %s", tag, mapName)
+		return fmt.Errorf("`%s` was not found in the %s", tag, mapName)
 	}
 
 	return nil
@@ -44,7 +44,7 @@ func InMap[M ~map[I]I, I comparable](tag, mapName string, item I, mp M) error {
 
 func MaxItems[T ~[]E, E any](tag string, set T, max int) error {
 	if len(set) > max {
-		return fmt.Errorf("%s can only have max %d elements", tag, max)
+		return fmt.Errorf("`%s` can only have max %d elements", tag, max)
 	}
 
 	return nil
@@ -57,7 +57,7 @@ func IsEmail(tag, s string, maxLen int) error {
 
 	em, err := mail.ParseAddress(s)
 	if err != nil || em.Address != s {
-		return fmt.Errorf("%s is not a valid e-mail", tag)
+		return fmt.Errorf("`%s` is not a valid e-mail", tag)
 	}
 
 	return nil
@@ -70,7 +70,7 @@ func IsURL(tag, u string, maxLen int) (*url.URL, error) {
 
 	p, err := url.Parse(u)
 	if err != nil || p.Host == "" || (p.Scheme != "https" && p.Scheme != "http") {
-		return nil, fmt.Errorf("%s is not a valid URL", tag)
+		return nil, fmt.Errorf("`%s` is not a valid URL", tag)
 	}
 
 	return p, nil
@@ -90,22 +90,22 @@ func WellKnownURL(tag string, manifest *url.URL, targetURL, wellKnownURL string,
 
 	// If there's a manifestURL, then targetURL should on the same domain. Otherwise, a well-known URL is mandatory.
 	if manifest.Host != tg.Host && wellKnownURL == "" {
-		return fmt.Errorf("%s.url and and manifest hostnames don't match. Provide %s.well-known", tag, tag)
+		return fmt.Errorf("`%s.url` and and manifest hostnames do not match. %s.wellKnown should be %s://%s%s%s", tag, tag, tg.Scheme, tg.Host, tg.Path, wellKnownPath)
 	}
 
 	// Validate its corresponding well known URL.
-	wk, err := IsURL(tag+".well-known", wellKnownURL, maxLen)
+	wk, err := IsURL(tag+".wellKnown", wellKnownURL, maxLen)
 	if err != nil {
 		return err
 	}
 
 	if !strings.HasSuffix(wk.Path, wellKnownPath) {
-		return fmt.Errorf("%s.well-known should end in %s", tag, wellKnownPath)
+		return fmt.Errorf("`%s.wellKnown` should end in %s", tag, wellKnownPath)
 	}
 
 	// well-known URL should match the main URL.
 	if wk.Host != tg.Host {
-		return fmt.Errorf("%s.url and %s.well-known hostnames don't match", tag, tag)
+		return fmt.Errorf("`%s.url` and `%s.wellKnown` hostnames do not match", tag, tag)
 	}
 
 	var (
@@ -115,7 +115,7 @@ func WellKnownURL(tag string, manifest *url.URL, targetURL, wellKnownURL string,
 
 	// If the base path is the root of the domain, then .well-known should also be.
 	if tgPath == "" && strings.TrimRight(wkPath, wellKnownPath) != "" {
-		return fmt.Errorf("%s.url and %s.well-known paths don't match", tag, tag)
+		return fmt.Errorf("`%s.url` and `%s.wellKnown` paths do not match. Should be %s://%s%s%s", tag, tag, tg.Scheme, tg.Host, tg.Path, wellKnownPath)
 	}
 
 	// If it's not at the root, then basePath should be a suffix of the well known path.
@@ -124,7 +124,7 @@ func WellKnownURL(tag string, manifest *url.URL, targetURL, wellKnownURL string,
 	// github.com/user/project ~= github.com/user/project/blob/main/.well-known/funding-json-urls
 	// github.com/use !~= github.com/user/project/blob/main/.well-known/funding-json-urls
 	if !strings.HasPrefix(wkPath, tgPath) || wkPath[len(tgPath)] != '/' {
-		return fmt.Errorf("%s.url and %s.well-known paths don't match", tag, tag)
+		return fmt.Errorf("`%s.url` and `%s.wellKnown` paths do not match", tag, tag)
 	}
 
 	return nil
@@ -137,7 +137,7 @@ func IsRepoURL(tag, u string) error {
 
 	p, err := url.Parse(u)
 	if err != nil || (p.Scheme != "https" && p.Scheme != "http" && p.Scheme != "git" && p.Scheme != "svn") {
-		return fmt.Errorf("%s is not a valid URL", p)
+		return fmt.Errorf("`%s` is not a valid URL", p)
 	}
 
 	return nil
@@ -148,7 +148,7 @@ func IsTag(tag string, val string, min, max int) error {
 		return err
 	}
 
-	err := fmt.Errorf("%s should be lowercase alpha-numeric-dashes and length %d - %d", tag, min, max)
+	err := fmt.Errorf("`%s` should be lowercase alpha-numeric-dashes and length %d - %d", tag, min, max)
 
 	if !reTag.MatchString(val) {
 		return err
@@ -166,7 +166,7 @@ func IsID(tag string, val string, min, max int) error {
 		return err
 	}
 
-	err := fmt.Errorf("%s should be lowercase alpha-numeric-dashes and length %d - %d", tag, min, max)
+	err := fmt.Errorf("`%s` should be lowercase alpha-numeric-dashes and length %d - %d", tag, min, max)
 
 	if !reID.MatchString(val) {
 		return err
